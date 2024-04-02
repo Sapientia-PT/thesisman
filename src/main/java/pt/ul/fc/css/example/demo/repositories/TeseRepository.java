1e3190f976c1986befd0a04ec051ba51c674ff56
fc58195 <fc58195@alunos.fc.ul.pt>
2024-04-02 23:54:13 +0100

    Added remaining repos

---
+++ src/main/java/pt/ul/fc/css/example/demo/repositories/TeseRepository.java
@@ -0,0 +1,17 @@
+package pt.ul.fc.css.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pt.ul.fc.css.example.demo.entities.Tema;
+import pt.ul.fc.css.example.demo.entities.Tese;
+
+import java.util.List;
+
+public interface TeseRepository extends JpaRepository<Tese, Long> {
+
+    Tese findByNrTese(float nrTese);
+
+    Tese findByTema(Tema tema);
+
+    List<Tese> findByNota(int nota);
+
+}
