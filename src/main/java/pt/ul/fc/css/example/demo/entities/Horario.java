3b69dc04b37d9857514319a27a6e7b9dc79449e8
fc58176 <fc58176@alunos.fc.ul.pt>
2024-04-03 11:45:29 +0100

    Added missing import and fixed equals

--- src/main/java/pt/ul/fc/css/example/demo/entities/Horario.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/Horario.java
@@ -1,82 +1,81 @@
 package pt.ul.fc.css.example.demo.entities;
 
 import jakarta.persistence.*;
-import org.springframework.lang.NonNull;
-
 import java.sql.Time;
+import java.util.Objects;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Horario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    @NonNull
-    private Time horaInicio;
-    @NonNull
-    private Time horaFim;
+  @NonNull private Time horaInicio;
+  @NonNull private Time horaFim;
 
-    @ManyToOne
-    private Sala sala;
+  @ManyToOne private Sala sala;
 
-    public Horario(@NonNull Time horaInicio, @NonNull Time horaFim, Sala sala) {
-        this.horaInicio = horaInicio;
-        this.horaFim = horaFim;
-        this.sala = sala;
-    }
+  public Horario(@NonNull Time horaInicio, @NonNull Time horaFim, Sala sala) {
+    this.horaInicio = horaInicio;
+    this.horaFim = horaFim;
+    this.sala = sala;
+  }
 
-    public Horario() {
+  public Horario() {}
 
-    }
+  @NonNull
+  public Time getHoraInicio() {
+    return horaInicio;
+  }
 
-    @NonNull
-    public Time getHoraInicio() {
-        return horaInicio;
-    }
+  public void setHoraInicio(@NonNull Time horaInicio) {
+    this.horaInicio = horaInicio;
+  }
 
-    public void setHoraInicio(@NonNull Time horaInicio) {
-        this.horaInicio = horaInicio;
-    }
+  @NonNull
+  public Time getHoraFim() {
+    return horaFim;
+  }
 
-    @NonNull
-    public Time getHoraFim() {
-        return horaFim;
-    }
+  public void setHoraFim(@NonNull Time horaFim) {
+    this.horaFim = horaFim;
+  }
 
-    public void setHoraFim(@NonNull Time horaFim) {
-        this.horaFim = horaFim;
-    }
+  public Sala getSala() {
+    return sala;
+  }
 
-    public Sala getSala() {
-        return sala;
-    }
+  public void setSala(Sala sala) {
+    this.sala = sala;
+  }
 
-    public void setSala(Sala sala) {
-        this.sala = sala;
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == this) return true;
+    if (obj == null || obj.getClass() != this.getClass()) return false;
+    var that = (Horario) obj;
+    return Objects.equals(this.horaInicio, that.horaInicio)
+        && Objects.equals(this.horaFim, that.horaFim)
+        && Objects.equals(this.sala, that.sala);
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this)
-            return true;
-        if (obj == null || obj.getClass() != this.getClass())
-            return false;
-        var that = (Aluno) obj;
-        return Objects.equals(this.horaInicio, that.horaInicio) &&
-                Objects.equals(this.horaFim, that.horaFim) &&
-                Objects.equals(this.sala, that.sala);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(horaInicio, horaFim, sala);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(horaInicio, horaFim, sala);
-    }
-
-    @Override
-    public String toString() {
-        return "Horario[" +
-                "horaInicio=" + horaInicio + ", " +
-                "horaFim=" + horaFim + ", " +
-                "sala=" + sala + ']';
-    }
+  @Override
+  public String toString() {
+    return "Horario["
+        + "horaInicio="
+        + horaInicio
+        + ", "
+        + "horaFim="
+        + horaFim
+        + ", "
+        + "sala="
+        + sala
+        + ']';
+  }
 }
