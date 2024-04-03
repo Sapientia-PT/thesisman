87a9bc98178b72aa354826e910bd0cf05632385a
fc58176 <fc58176@alunos.fc.ul.pt>
2024-04-03 11:47:56 +0100

    Just for cohesion reasons

--- src/main/java/pt/ul/fc/css/example/demo/entities/Mestrado.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/Mestrado.java
@@ -1,48 +1,42 @@
 package pt.ul.fc.css.example.demo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.lang.NonNull;
 
 @Entity
 public class Mestrado {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    @NonNull
-    private long idMestrado;
+  @NonNull private long idMestrado;
 
-    @Column(unique = true)
-    @NonNull
-    private String nome;
+  @Column(unique = true)
+  @NonNull
+  private String nome;
 
-    public Mestrado(long idMestrado, @NonNull String nome) {
-        this.idMestrado = idMestrado;
-        this.nome = nome;
-    }
+  public Mestrado(long idMestrado, @NonNull String nome) {
+    this.idMestrado = idMestrado;
+    this.nome = nome;
+  }
 
-    public Mestrado() {
+  public Mestrado() {}
 
-    }
+  public long getIdMestrado() {
+    return idMestrado;
+  }
 
-    public long getIdMestrado() {
-        return idMestrado;
-    }
+  public void setIdMestrado(long idMestrado) {
+    this.idMestrado = idMestrado;
+  }
 
-    public void setIdMestrado(long idMestrado) {
-        this.idMestrado = idMestrado;
-    }
+  @NonNull
+  public String getNome() {
+    return nome;
+  }
 
-    @NonNull
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(@NonNull String nome) {
-        this.nome = nome;
-    }
+  public void setNome(@NonNull String nome) {
+    this.nome = nome;
+  }
 }
