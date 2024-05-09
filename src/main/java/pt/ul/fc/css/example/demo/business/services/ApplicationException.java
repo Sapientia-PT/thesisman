ae400ac5929c539c401807fbededc1557282a2f4
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 01:27:08 +0100

    Add exceptions

---
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/ApplicationException.java
@@ -0,0 +1,7 @@
+package pt.ul.fc.css.example.demo.business.services;
+
+public class ApplicationException extends Exception {
+    public ApplicationException(String message){
+        super(message);
+    }
+}
