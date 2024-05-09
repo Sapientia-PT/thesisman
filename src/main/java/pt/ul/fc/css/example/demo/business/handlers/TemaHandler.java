b66580f4064a574224a00c93e33149dae89178f8
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 01:27:53 +0100

    Switch to Component and add create method

--- src/main/java/pt/ul/fc/css/example/demo/business/handlers/TemaHandler.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/handlers/TemaHandler.java
@@ -1,15 +1,26 @@
 package pt.ul.fc.css.example.demo.business.handlers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import pt.ul.fc.css.example.demo.business.repository.TemaRepository;
+import pt.ul.fc.css.example.demo.business.services.NullTitleException;
+import pt.ul.fc.css.example.demo.entities.Tema;
 
-@Service
+@Component
 public class TemaHandler {
 
     @Autowired
     private TemaRepository temaRepository;
 
+    public Tema createTema(String titulo, String descricao, float remunMensal) throws NullTitleException {
+        if(titulo.isEmpty())
+            throw new NullTitleException("Titulo is a required field");
 
+        Tema tema = new Tema();
+        tema.setTitulo(titulo);
+        tema.setDescricao(descricao);
+        tema.setRemunMensal(remunMensal);
+        return temaRepository.save(tema);
+    }
 
 }
