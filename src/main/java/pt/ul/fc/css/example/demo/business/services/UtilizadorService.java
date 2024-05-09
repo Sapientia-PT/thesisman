b81dff1252e1296dbb7e3b947dff4e4b66c7a853
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 18:19:59 +0100

    Implement rought sketch for UtilizadorService

--- src/main/java/pt/ul/fc/css/example/demo/business/services/UtilizadorService.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/UtilizadorService.java
@@ -1,4 +1,27 @@
 package pt.ul.fc.css.example.demo.business.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pt.ul.fc.css.example.demo.business.handlers.AlunoHandler;
+import pt.ul.fc.css.example.demo.business.repository.AlunoRepository;
+import pt.ul.fc.css.example.demo.business.services.DTOs.AlunoDTO;
+
+import java.util.List;
+
+@Service
 public class UtilizadorService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+    @Autowired
+    private AlunoHandler alunoHandler;
+
+    public List<AlunoDTO> getAlunos(){
+        return alunoHandler.getAlunos();
+    }
+
+    public AlunoDTO createAluno(int nrAluno, String nome, float media){
+        return alunoHandler.createAluno(nrAluno, nome, media);
+    }
+
 }
