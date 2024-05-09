6fb641ee787c1e5292e07f3e0fcc338da85fb69c
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 18:20:27 +0100

    Simplify TemaService as handler now has CRUD

--- src/main/java/pt/ul/fc/css/example/demo/business/services/TemaService.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/TemaService.java
@@ -2,10 +2,14 @@ package pt.ul.fc.css.example.demo.business.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pt.ul.fc.css.example.demo.business.repository.AlunoRepository;
 import pt.ul.fc.css.example.demo.business.repository.TemaRepository;
 import pt.ul.fc.css.example.demo.business.handlers.TemaHandler;
+import pt.ul.fc.css.example.demo.business.services.DTOs.AlunoDTO;
 import pt.ul.fc.css.example.demo.business.services.DTOs.TemaDTO;
+import pt.ul.fc.css.example.demo.business.services.Exceptions.ApplicationException;
 import pt.ul.fc.css.example.demo.business.services.Exceptions.NullTitleException;
+import pt.ul.fc.css.example.demo.entities.Aluno;
 import pt.ul.fc.css.example.demo.entities.Tema;
 
 import java.util.ArrayList;
@@ -17,60 +21,37 @@ public class TemaService {
 
     @Autowired
     private TemaRepository temaRepository;
-
     @Autowired
     private TemaHandler temaHandler;
 
+    @Autowired
+    private AlunoRepository alunoRepository;
+
     public TemaDTO createTema(String titulo, String descricao, float remunMensal) throws NullTitleException {
-        Tema tema = temaHandler.createTema(titulo, descricao, remunMensal);
-        return TemaService.dtofy(tema);
+        return temaHandler.createTema(titulo, descricao, remunMensal);
     }
 
-    public Optional<TemaDTO> getTema(Long id) {
-        return temaRepository.findById(id).map(TemaService::dtofy);
+    public List<TemaDTO> getTemas(){
+        return temaHandler.getTemas();
     }
 
-    public List<TemaDTO> getTemas() {
-        ArrayList<TemaDTO> temas = new ArrayList<>();
-        for (Tema tema : temaRepository.findAll())
-            temas.add(dtofy(tema));
-        return temas;
-    }
+    public void atribuirTemaAluno(TemaDTO temaDTO, AlunoDTO alunoDTO) throws ApplicationException {
+        Optional<Aluno> optionalAluno = alunoRepository.findById(alunoDTO.getId());
+        if (!optionalAluno.isPresent())
+            throw new ApplicationException("Aluno not found");
 
-    public void deleteTema(Long id){
-        temaRepository.deleteById(id);
-    }
+        Optional<Tema> optionalTema = temaRepository.findById(temaDTO.getId());
+        if (!optionalTema.isPresent()) {
+            throw new ApplicationException("Tema not found");
+        }
 
-    public Optional<TemaDTO> replaceTema(Long id, TemaDTO newTemaData) {
-        Optional<Tema> oldTema = temaRepository.findById(id);
-        oldTema.ifPresent(tema -> {
-            tema.setTitulo(newTemaData.getTitulo());
-            tema.setDescricao(newTemaData.getDescricao());
-            tema.setRemunMensal(newTemaData.getRemunMensal());
-            temaRepository.save(tema);
-        });
-        return oldTema.map(TemaService::dtofy);
-    }
+        Aluno aluno = optionalAluno.get();
+        Tema tema = optionalTema.get();
 
-    public Optional<TemaDTO> changeTema(Long id, TemaDTO newTemaData) {
-        Optional<Tema> oldTema = temaRepository.findById(id);
-        oldTema.ifPresent(tema -> {
-            if (newTemaData.getTitulo() != null)
-                tema.setTitulo(newTemaData.getTitulo());
-            if (newTemaData.getDescricao() != null)
-                tema.setDescricao(newTemaData.getDescricao());
-            tema.setRemunMensal(newTemaData.getRemunMensal());
-            temaRepository.save(tema);
-        });
-        return oldTema.map(TemaService::dtofy);
-    }
+        aluno.getTemasEscolhidos().add(tema);
+        tema.setAluno(aluno);
 
-    private static TemaDTO dtofy(Tema c) {
-        TemaDTO temaDTO = new TemaDTO();
-        temaDTO.setId(c.getId());
-        temaDTO.setTitulo(c.getTitulo());
-        temaDTO.setDescricao(c.getDescricao());
-        temaDTO.setRemunMensal(c.getRemunMensal());
-        return temaDTO;
+        alunoRepository.save(aluno);
+        temaRepository.save(tema);
     }
 }
