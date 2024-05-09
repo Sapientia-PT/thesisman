17125425ac311cc95b7767f579414092ae35b494
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 18:19:05 +0100

    Pass CRUD to handler and leave logic to service

--- src/main/java/pt/ul/fc/css/example/demo/business/handlers/TemaHandler.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/handlers/TemaHandler.java
@@ -3,8 +3,14 @@ package pt.ul.fc.css.example.demo.business.handlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pt.ul.fc.css.example.demo.business.repository.TemaRepository;
+import pt.ul.fc.css.example.demo.business.services.DTOs.TemaDTO;
 import pt.ul.fc.css.example.demo.business.services.Exceptions.NullTitleException;
+import pt.ul.fc.css.example.demo.business.services.TemaService;
 import pt.ul.fc.css.example.demo.entities.Tema;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TemaHandler {
@@ -12,7 +18,7 @@ public class TemaHandler {
     @Autowired
     private TemaRepository temaRepository;
 
-    public Tema createTema(String titulo, String descricao, float remunMensal) throws NullTitleException {
+    public TemaDTO createTema(String titulo, String descricao, float remunMensal) throws NullTitleException {
         if(titulo.isEmpty())
             throw new NullTitleException("Titulo is a required field");
 
@@ -20,7 +26,57 @@ public class TemaHandler {
         tema.setTitulo(titulo);
         tema.setDescricao(descricao);
         tema.setRemunMensal(remunMensal);
-        return temaRepository.save(tema);
+
+        return dtofy(temaRepository.save(tema));
+    }
+
+    public Optional<TemaDTO> getTema(Long id) {
+        return temaRepository.findById(id).map(this::dtofy);
+    }
+
+    public List<TemaDTO> getTemas() {
+        ArrayList<TemaDTO> temas = new ArrayList<>();
+        for (Tema tema : temaRepository.findAll())
+            temas.add(dtofy(tema));
+        return temas;
+    }
+
+    public void deleteTema(Long id){
+        temaRepository.deleteById(id);
+    }
+
+    public Optional<TemaDTO> replaceTema(Long id, TemaDTO newTemaData) {
+        Optional<Tema> oldTema = temaRepository.findById(id);
+        oldTema.ifPresent(tema -> {
+            tema.setTitulo(newTemaData.getTitulo());
+            tema.setDescricao(newTemaData.getDescricao());
+            tema.setRemunMensal(newTemaData.getRemunMensal());
+            temaRepository.save(tema);
+        });
+        return oldTema.map(this::dtofy);
+    }
+
+    public Optional<TemaDTO> changeTema(Long id, TemaDTO newTemaData) {
+        Optional<Tema> oldTema = temaRepository.findById(id);
+        oldTema.ifPresent(tema -> {
+            if (newTemaData.getTitulo() != null)
+                tema.setTitulo(newTemaData.getTitulo());
+            if (newTemaData.getDescricao() != null)
+                tema.setDescricao(newTemaData.getDescricao());
+            tema.setRemunMensal(newTemaData.getRemunMensal());
+            temaRepository.save(tema);
+        });
+        return oldTema.map(this::dtofy);
+    }
+
+    private TemaDTO dtofy(Tema c) {
+        TemaDTO temaDTO = new TemaDTO();
+        temaDTO.setId(c.getId());
+        temaDTO.setTitulo(c.getTitulo());
+        temaDTO.setDescricao(c.getDescricao());
+        temaDTO.setRemunMensal(c.getRemunMensal());
+        temaDTO.setAluno(c.getAluno());
+        return temaDTO;
     }
 
 }
