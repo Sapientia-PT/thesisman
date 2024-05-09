76031cf0deeaea6b6e00910468a9f021b5b23cc4
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 12:22:24 +0100

    Example usage to test controller working with TemaService

--- src/main/java/pt/ul/fc/css/example/demo/WebController.java
+++ src/main/java/pt/ul/fc/css/example/demo/WebController.java
@@ -1,14 +1,33 @@
 package pt.ul.fc.css.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pt.ul.fc.css.example.demo.business.services.NullTitleException;
+import pt.ul.fc.css.example.demo.business.services.TemaService;
 
 @Controller
 public class WebController {
 
+    @Autowired
+    TemaService temaService;
+
     @RequestMapping("/")
     public String getIndex(Model model) {
+        model.addAttribute("temas", temaService.getTemas());
         return "index";
+    }
+
+
+    @RequestMapping("/init")
+    public String initTest(Model model) {
+        try{
+            temaService.createTema("Republica das bananas", "Bananas!", 1000);
+            temaService.createTema("Macacos", "Ooga Booga", 42);
+            return "init";
+        } catch (NullTitleException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
