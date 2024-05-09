cc58371dd56ef0b7b97ce391643b939750425b35
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-09 10:49:28 +0100

    Give Entity basic properties

--- src/main/java/pt/ul/fc/css/example/demo/entities/Administrador.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/Administrador.java
@@ -1,4 +1,26 @@
 package pt.ul.fc.css.example.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Administrador {
+    @Id
+    private Long id;
+
+    public Administrador(Long id) {
+        this.id = id;
+    }
+
+    public Administrador() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
