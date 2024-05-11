f0bfe8a798f5fcecf3186716c732bdba4de1ea7e
fc58176 <fc58176@alunos.fc.ul.pt>
2024-05-11 09:11:59 +0100

    refactor: Rename temasEscolhidos to temasCandidatados in AlunoDTO (same name as used in Aluno)

--- src/main/java/pt/ul/fc/css/example/demo/business/services/DTOs/AlunoDTO.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/DTOs/AlunoDTO.java
@@ -1,71 +1,70 @@
 package pt.ul.fc.css.example.demo.business.services.DTOs;
 
+import java.util.List;
 import org.springframework.stereotype.Component;
 import pt.ul.fc.css.example.demo.entities.Tema;
 import pt.ul.fc.css.example.demo.entities.Tese;
 
-import java.util.List;
-
 @Component
 public class AlunoDTO {
 
-    private long Id;
+  private long Id;
 
-    public long getId() {
-        return Id;
-    }
+  public long getId() {
+    return Id;
+  }
 
-    public void setId(long id) {
-        Id = id;
-    }
+  public void setId(long id) {
+    Id = id;
+  }
 
-    private int nrAluno;
+  private int nrAluno;
 
-    private String nome;
+  private String nome;
 
-    private float media;
+  private float media;
 
-    private Tese tese;
+  private Tese tese;
 
-    private List<Tema> temasEscolhidos;
+  private List<Tema> temasCandidatados;
 
-    public int getNrAluno() {
-        return nrAluno;
-    }
+  public int getNrAluno() {
+    return nrAluno;
+  }
 
-    public void setNrAluno(int nrAluno) {
-        this.nrAluno = nrAluno;
-    }
+  public void setNrAluno(int nrAluno) {
+    this.nrAluno = nrAluno;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public float getMedia() {
-        return media;
-    }
+  public float getMedia() {
+    return media;
+  }
 
-    public void setMedia(float media) {
-        this.media = media;
-    }
+  public void setMedia(float media) {
+    this.media = media;
+  }
 
-    public Tese getTese() {
-        return tese;
-    }
+  public Tese getTese() {
+    return tese;
+  }
 
-    public void setTese(Tese tese) {
-        this.tese = tese;
-    }
+  public void setTese(Tese tese) {
+    this.tese = tese;
+  }
 
-    public List<Tema> getTemasEscolhidos() {
-        return temasEscolhidos;
-    }
+  public List<Tema> getTemasCandidatados() {
+    return temasCandidatados;
+  }
 
-    public void setTemasEscolhidos(List<Tema> temasEscolhidos) {
-        this.temasEscolhidos = temasEscolhidos;
-    }
+  public void setTemasCandidatados(List<Tema> temasCandidatados) {
+    this.temasCandidatados = temasCandidatados;
+  }
 }
