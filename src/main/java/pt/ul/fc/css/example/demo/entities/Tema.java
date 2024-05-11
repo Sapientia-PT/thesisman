cff3d05609b2b45d1afb4f73572174a9c4089998
fc58176 <fc58176@alunos.fc.ul.pt>
2024-05-11 12:13:23 +0100

    refactor: @NonNull annotation usage simplification

--- src/main/java/pt/ul/fc/css/example/demo/entities/Tema.java
+++ src/main/java/pt/ul/fc/css/example/demo/entities/Tema.java
@@ -6,69 +6,66 @@ import org.springframework.lang.NonNull;
 @Entity
 public class Tema {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
 
-    private String titulo;
+  @NonNull private String titulo;
 
-    private String descricao;
+  private String descricao;
 
-    private float remunMensal;
+  private float remunMensal;
 
-    @ManyToOne
-    @JoinColumn(name = "aluno_id")
-    private Aluno aluno;
+  @ManyToOne
+  @JoinColumn(name = "aluno_id")
+  private Aluno aluno;
 
-    public Tema(@NonNull String titulo, String descricao, float remunMensal, Aluno aluno) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.remunMensal = remunMensal;
-        this.aluno = aluno;
-    }
+  public Tema(String titulo, String descricao, float remunMensal, Aluno aluno) {
+    this.titulo = titulo;
+    this.descricao = descricao;
+    this.remunMensal = remunMensal;
+    this.aluno = aluno;
+  }
 
-    public Tema() {
+  public Tema() {}
 
-    }
+  public long getId() {
+    return id;
+  }
 
-    public long getId() {
-        return id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public String getTitulo() {
+    return titulo;
+  }
 
-    @NonNull
-    public String getTitulo() {
-        return titulo;
-    }
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
 
-    public void setTitulo(@NonNull String titulo) {
-        this.titulo = titulo;
-    }
+  public String getDescricao() {
+    return descricao;
+  }
 
-    public String getDescricao() {
-        return descricao;
-    }
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+  public float getRemunMensal() {
+    return remunMensal;
+  }
 
-    public float getRemunMensal() {
-        return remunMensal;
-    }
+  public void setRemunMensal(float remunMensal) {
+    this.remunMensal = remunMensal;
+  }
 
-    public void setRemunMensal(float remunMensal) {
-        this.remunMensal = remunMensal;
-    }
+  public Aluno getAluno() {
+    return aluno;
+  }
 
-    public Aluno getAluno() {
-        return aluno;
-    }
-
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
+  public void setAluno(Aluno aluno) {
+    this.aluno = aluno;
+  }
 }
