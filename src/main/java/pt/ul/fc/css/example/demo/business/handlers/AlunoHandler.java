abbbdfe662e396069fc489dc422317e262e1c289
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-13 16:33:05 +0100

    Similarly than for Tema, put logic in Service and leave Handler for use cases

--- src/main/java/pt/ul/fc/css/example/demo/business/handlers/AlunoHandler.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/handlers/AlunoHandler.java
@@ -1,12 +1,9 @@
 package pt.ul.fc.css.example.demo.business.handlers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pt.ul.fc.css.example.demo.business.repository.AlunoRepository;
-import pt.ul.fc.css.example.demo.business.services.DTOs.AlunoDTO;
 import pt.ul.fc.css.example.demo.entities.Aluno;
 
 @Component
@@ -14,34 +11,17 @@ public class AlunoHandler {
 
   @Autowired private AlunoRepository alunoRepository;
 
-  public AlunoDTO createAluno(int nrAluno, String nome, float media) {
+  public Aluno createAluno(int nrAluno, String nome, float media) {
 
     Aluno aluno = new Aluno();
     aluno.setNrAluno(nrAluno);
     aluno.setNome(nome);
     aluno.setMedia(media);
 
-    return dtofy(alunoRepository.save(aluno));
+    return alunoRepository.save(aluno);
   }
 
-  public Optional<AlunoDTO> getAluno(Long id) {
-    return alunoRepository.findById(id).map(this::dtofy);
-  }
-
-  public List<AlunoDTO> getAlunos() {
-    ArrayList<AlunoDTO> alunos = new ArrayList<>();
-    for (Aluno aluno : alunoRepository.findAll()) alunos.add(dtofy(aluno));
-    return alunos;
-  }
-
-  private AlunoDTO dtofy(Aluno a) {
-    AlunoDTO alunoDTO = new AlunoDTO();
-    alunoDTO.setId(a.getId());
-    alunoDTO.setNrAluno(a.getNrAluno());
-    alunoDTO.setNome(a.getNome());
-    alunoDTO.setMedia(a.getMedia());
-    alunoDTO.setTese(a.getTese());
-    alunoDTO.setTemasCandidatados(a.getTemasCandidatados());
-    return alunoDTO;
+  public List<Aluno> getAlunos() {
+    return alunoRepository.findAll();
   }
 }
--- src/main/java/pt/ul/fc/css/example/demo/business/services/UtilizadorService.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/UtilizadorService.java
@@ -1,27 +1,37 @@
 package pt.ul.fc.css.example.demo.business.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pt.ul.fc.css.example.demo.business.handlers.AlunoHandler;
 import pt.ul.fc.css.example.demo.business.repository.AlunoRepository;
 import pt.ul.fc.css.example.demo.business.services.DTOs.AlunoDTO;
-
-import java.util.List;
+import pt.ul.fc.css.example.demo.entities.Aluno;
 
 @Service
 public class UtilizadorService {
 
-    @Autowired
-    private AlunoRepository alunoRepository;
-    @Autowired
-    private AlunoHandler alunoHandler;
+  @Autowired private AlunoRepository alunoRepository;
 
-    public List<AlunoDTO> getAlunos(){
-        return alunoHandler.getAlunos();
-    }
+  @Autowired private AlunoHandler alunoHandler;
 
-    public AlunoDTO createAluno(int nrAluno, String nome, float media){
-        return alunoHandler.createAluno(nrAluno, nome, media);
-    }
+  public List<AlunoDTO> getAlunos() {
+    return alunoHandler.getAlunos().stream().map(this::dtofy).collect(Collectors.toList());
+  }
 
+  public AlunoDTO createAluno(int nrAluno, String nome, float media) {
+    return dtofy(alunoHandler.createAluno(nrAluno, nome, media));
+  }
+
+  private AlunoDTO dtofy(Aluno a) {
+    AlunoDTO alunoDTO = new AlunoDTO();
+    alunoDTO.setId(a.getId());
+    alunoDTO.setNrAluno(a.getNrAluno());
+    alunoDTO.setNome(a.getNome());
+    alunoDTO.setMedia(a.getMedia());
+    alunoDTO.setTese(a.getTese());
+    alunoDTO.setTemasCandidatados(a.getTemasCandidatados());
+    return alunoDTO;
+  }
 }
