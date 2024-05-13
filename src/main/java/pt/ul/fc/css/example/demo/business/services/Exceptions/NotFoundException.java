2930658ebc4b3e342ecd9d497cbd0f4cbb71e805
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-13 15:05:17 +0100

    One more needed exception

---
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/Exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package pt.ul.fc.css.example.demo.business.services.Exceptions;
+
+public class NotFoundException extends ApplicationException {
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+}
