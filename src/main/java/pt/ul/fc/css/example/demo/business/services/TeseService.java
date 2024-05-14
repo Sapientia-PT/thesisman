43ff57facbf90b302b139cafcf118228267a0663
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-14 14:35:11 +0100

    Skeleton for first use case

--- src/main/java/pt/ul/fc/css/example/demo/business/services/TeseService.java
+++ src/main/java/pt/ul/fc/css/example/demo/business/services/TeseService.java
@@ -1,8 +1,11 @@
 package pt.ul.fc.css.example.demo.business.services;
 
 import org.springframework.stereotype.Service;
+import pt.ul.fc.css.example.demo.business.services.DTOs.AlunoDTO;
+import pt.ul.fc.css.example.demo.business.services.DTOs.TemaDTO;
 
 @Service
 public class TeseService {
 
+  public void atribuirTemaALuno(TemaDTO temaDTO, AlunoDTO alunoDTO) {}
 }
