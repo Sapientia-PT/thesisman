4139df5c78d0b6c3e15fa9c72b54ea6f16111f95
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-17 22:43:42 +0100

    add com.google.gson dependency to read from REST API.

--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -3,7 +3,10 @@ module pt.ul.fc.css.alunosfx {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires com.google.gson;
 
     opens pt.ul.fc.css.alunosfx to javafx.fxml;
+    opens pt.ul.fc.css.alunosfx.presentation.model to com.google.gson;
     exports pt.ul.fc.css.alunosfx;
+    exports pt.ul.fc.css.alunosfx.presentation.model to com.google.gson;
 }
