377b90efab8d7d4748ca8ddc8975b178884f86cc
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-18 12:46:55 +0100

    add TemaRead, will probably use

---
+++ client/src/main/java/pt/ul/fc/css/alunosfx/presentation/model/TemaRead.java
@@ -0,0 +1,32 @@
+package pt.ul.fc.css.alunosfx.presentation.model;
+
+public class TemaRead {
+
+  private String titulo;
+  private String descricao;
+  private float remunMensal;
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public float getRemunMensal() {
+    return remunMensal;
+  }
+
+  public void setRemunMensal(float remunMensal) {
+    this.remunMensal = remunMensal;
+  }
+}
