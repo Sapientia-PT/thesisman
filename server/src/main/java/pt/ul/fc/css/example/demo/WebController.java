73b5c34244214a4699ae61065cc93c40e26c8570
fc58176 <fc58176@alunos.fc.ul.pt>
2024-05-18 09:30:02 +0100

    Added login functionality to WebController

--- server/src/main/java/pt/ul/fc/css/example/demo/WebController.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/WebController.java
@@ -3,7 +3,9 @@ package pt.ul.fc.css.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pt.ul.fc.css.example.demo.business.services.DTOs.TemaDTO;
 import pt.ul.fc.css.example.demo.business.services.EstatisticaService;
 import pt.ul.fc.css.example.demo.business.services.Exceptions.ApplicationException;
@@ -19,7 +21,27 @@ public class WebController {
 
   @RequestMapping("/")
   public String getIndex(Model model) {
-    return "redirect:/temas";
+    return "redirect:/login";
+  }
+
+  @RequestMapping("/login")
+  public String login(Model model) {
+    return "login";
+  }
+
+  @PostMapping("/doLogin")
+  public String doLogin(
+      @RequestParam("username") String username,
+      @RequestParam("password") String password,
+      Model model) {
+    // mock login validation: accept any password, but validate if username exists in the database
+    if (!username.isEmpty() && !password.isEmpty()) { // TODO Just for now
+      // TODO add user details to the model if needed
+      model.addAttribute("username", username);
+      return "redirect:/temas"; // TODO change this to the main page
+    }
+    // If login fails, redirect back to the login page
+    return "redirect:/login";
   }
 
   @RequestMapping("/alunos")
