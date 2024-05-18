28f62aca916fa3843fe90ced78fd401a3d2e4338
fc58195 <fc58195@alunos.fc.ul.pt>
2024-05-18 12:55:16 +0100

    sketch for get temas

--- server/src/main/java/pt/ul/fc/css/example/demo/RestAPI.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/RestAPI.java
@@ -1,22 +1,30 @@
 package pt.ul.fc.css.example.demo;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.ul.fc.css.example.demo.business.services.DTOs.AlunoDTO;
+import pt.ul.fc.css.example.demo.business.services.DTOs.TemaDTO;
+import pt.ul.fc.css.example.demo.business.services.TemaService;
 import pt.ul.fc.css.example.demo.business.services.UtilizadorService;
 
 @RestController
 @RequestMapping("/api")
 public class RestAPI {
 
-    @Autowired
-    private UtilizadorService utilizadorService;
-    @GetMapping("/aluno/{nrAluno}")
-    public AlunoDTO getAluno(@PathVariable("nrAluno") int nrAluno) {
-        return utilizadorService.getAluno(nrAluno);
-    }
+  @Autowired private UtilizadorService utilizadorService;
+  @Autowired private TemaService temaService;
 
+  @GetMapping("/aluno/{nrAluno}")
+  public AlunoDTO getAluno(@PathVariable("nrAluno") int nrAluno) {
+    return utilizadorService.getAluno(nrAluno);
+  }
+
+  @GetMapping("/temas")
+  public List<TemaDTO> getTemas() {
+    return temaService.getTemas();
+  }
 }
--- server/src/main/java/pt/ul/fc/css/example/demo/business/services/DTOs/TemaDTO.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/business/services/DTOs/TemaDTO.java
@@ -1,56 +1,55 @@
 package pt.ul.fc.css.example.demo.business.services.DTOs;
 
 import org.springframework.stereotype.Component;
-import pt.ul.fc.css.example.demo.entities.Aluno;
 
 @Component
 public class TemaDTO {
 
-    private long Id;
+  private long Id;
 
-    public long getId() {
-        return Id;
-    }
+  public long getId() {
+    return Id;
+  }
 
-    public void setId(long id) {
-        Id = id;
-    }
+  public void setId(long id) {
+    Id = id;
+  }
 
-    private String titulo;
-    private String descricao;
-    private float remunMensal;
+  private String titulo;
+  private String descricao;
+  private float remunMensal;
 
-    private Aluno aluno;
+  private AlunoDTO alunoDTO;
 
-    public String getTitulo() {
-        return titulo;
-    }
+  public String getTitulo() {
+    return titulo;
+  }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
 
-    public String getDescricao() {
-        return descricao;
-    }
+  public String getDescricao() {
+    return descricao;
+  }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
 
-    public float getRemunMensal() {
-        return remunMensal;
-    }
+  public float getRemunMensal() {
+    return remunMensal;
+  }
 
-    public void setRemunMensal(float remunMensal) {
-        this.remunMensal = remunMensal;
-    }
+  public void setRemunMensal(float remunMensal) {
+    this.remunMensal = remunMensal;
+  }
 
-    public Aluno getAluno() {
-        return aluno;
-    }
+  public AlunoDTO getAluno() {
+    return alunoDTO;
+  }
 
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
+  public void setAluno(AlunoDTO alunoDTO) {
+    this.alunoDTO = alunoDTO;
+  }
 }
