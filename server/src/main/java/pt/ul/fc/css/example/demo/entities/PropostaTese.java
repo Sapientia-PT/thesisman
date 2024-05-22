44f1da7d7013875844197458be5df9f90e506a36
fc58236 <fc58236@alunos.fc.ul.pt>
2024-05-22 01:42:21 +0100

    Refactor PropostaTese entity and add author comments

--- server/src/main/java/pt/ul/fc/css/example/demo/entities/PropostaTese.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/entities/PropostaTese.java
@@ -2,6 +2,13 @@ package pt.ul.fc.css.example.demo.entities;
 
 import jakarta.persistence.*;
 
+/**
+ * This class represents an PropostaTese entity.
+ *
+ * @author Joao Miguel
+ * @author Guilherme Gouveia
+ * @author Rafael Correia
+ */
 @Entity
 public class PropostaTese {
   @Id
