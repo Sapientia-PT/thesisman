7ca7d310b6408d081af8b0b433f6ba74bdbf60ce
fc58236 <fc58236@alunos.fc.ul.pt>
2024-05-22 20:38:42 +0100

    Refactor JuriRepository and add author comments

--- server/src/main/java/pt/ul/fc/css/example/demo/business/repository/JuriRepository.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/business/repository/JuriRepository.java
@@ -4,6 +4,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pt.ul.fc.css.example.demo.entities.*;
 
+/**
+ * This class reprents the Juri Repository.
+ *
+ * @author Joao Miguel
+ * @author Guilherme Gouveia
+ * @author Rafael Correia
+ */
 public interface JuriRepository extends JpaRepository<Juri, Long> {
 
   Juri findByDefesa(Defesa defesa);
