504d3e5c8143ee0afb61b9c0411f0d20ee6c01e5
fc58236 <fc58236@alunos.fc.ul.pt>
2024-05-22 01:38:28 +0100

    Refactor Dissertacao entity and add author comments

--- server/src/main/java/pt/ul/fc/css/example/demo/entities/Dissertacao.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/entities/Dissertacao.java
@@ -2,7 +2,12 @@ package pt.ul.fc.css.example.demo.entities;
 
 import jakarta.persistence.Entity;
 
+/**
+ * This class represents an dissertacao entity.
+ *
+ * @author Joao Miguel
+ * @author Guilherme Gouveia
+ * @author Rafael Correia
+ */
 @Entity
-public class Dissertacao extends Tese {
-
-}
+public class Dissertacao extends Tese {}
