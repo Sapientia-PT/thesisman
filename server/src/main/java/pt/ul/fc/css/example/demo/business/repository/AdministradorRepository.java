4b0b7a1af8a41fa074d4f5638f0a51b055c24d60
fc58236 <fc58236@alunos.fc.ul.pt>
2024-05-22 02:00:31 +0100

    Refactor docs

--- server/src/main/java/pt/ul/fc/css/example/demo/business/repository/AdministradorRepository.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/business/repository/AdministradorRepository.java
@@ -3,6 +3,13 @@ package pt.ul.fc.css.example.demo.business.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pt.ul.fc.css.example.demo.entities.Administrador;
 
+/**
+ * This class reprents the Administrator Repository.
+ *
+ * @author Joao Miguel
+ * @author Guilherme Gouveia
+ * @author Rafael Correia
+ */
 public interface AdministradorRepository extends JpaRepository<Administrador, Long> {
 
   Administrador findByNrConta(long nrAdministrador);
