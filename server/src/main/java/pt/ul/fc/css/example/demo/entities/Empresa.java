4d1fea37086de5675fc61a7cc38893c618c42c44
fc58236 <fc58236@alunos.fc.ul.pt>
2024-05-22 01:39:26 +0100

    Refactor Empresa entity and add author comments

--- server/src/main/java/pt/ul/fc/css/example/demo/entities/Empresa.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/entities/Empresa.java
@@ -1,66 +1,71 @@
 package pt.ul.fc.css.example.demo.entities;
 
 import jakarta.persistence.*;
+import java.util.List;
 import org.springframework.lang.NonNull;
 
-import java.util.List;
-
+/**
+ * This class represents an empresa entity.
+ *
+ * @author Joao Miguel
+ * @author Guilherme Gouveia
+ * @author Rafael Correia
+ */
 @Entity
 public class Empresa {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
 
-    @Column(unique = true, nullable = false)
-    private long nrEmpresa;
+  @Column(unique = true, nullable = false)
+  private long nrEmpresa;
 
-    @Column(unique = true)
-    private String nome;
+  @Column(unique = true)
+  private String nome;
 
-    @OneToMany(mappedBy = "empresa")
-    private List<OrientadorExterno> orientadoresExternos;
+  @OneToMany(mappedBy = "empresa")
+  private List<OrientadorExterno> orientadoresExternos;
 
-    public Empresa(long nrEmpresa, @NonNull String nome, List<OrientadorExterno> orientadoresExternos) {
-        this.nrEmpresa = nrEmpresa;
-        this.nome = nome;
-        this.orientadoresExternos = orientadoresExternos;
-    }
+  public Empresa(
+      long nrEmpresa, @NonNull String nome, List<OrientadorExterno> orientadoresExternos) {
+    this.nrEmpresa = nrEmpresa;
+    this.nome = nome;
+    this.orientadoresExternos = orientadoresExternos;
+  }
 
-    public Empresa() {
+  public Empresa() {}
 
-    }
+  public long getId() {
+    return id;
+  }
 
-    public long getId() {
-        return id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public long getNrEmpresa() {
+    return nrEmpresa;
+  }
 
-    public long getNrEmpresa() {
-        return nrEmpresa;
-    }
+  public void setNrEmpresa(long nrEmpresa) {
+    this.nrEmpresa = nrEmpresa;
+  }
 
-    public void setNrEmpresa(long nrEmpresa) {
-        this.nrEmpresa = nrEmpresa;
-    }
+  @NonNull
+  public String getNome() {
+    return nome;
+  }
 
-    @NonNull
-    public String getNome() {
-        return nome;
-    }
+  public void setNome(@NonNull String nome) {
+    this.nome = nome;
+  }
 
-    public void setNome(@NonNull String nome) {
-        this.nome = nome;
-    }
+  public List<OrientadorExterno> getOrientadoresExternos() {
+    return orientadoresExternos;
+  }
 
-    public List<OrientadorExterno> getOrientadoresExternos() {
-        return orientadoresExternos;
-    }
-
-    public void setOrientadoresExternos(List<OrientadorExterno> orientadoresExternos) {
-        this.orientadoresExternos = orientadoresExternos;
-    }
+  public void setOrientadoresExternos(List<OrientadorExterno> orientadoresExternos) {
+    this.orientadoresExternos = orientadoresExternos;
+  }
 }
