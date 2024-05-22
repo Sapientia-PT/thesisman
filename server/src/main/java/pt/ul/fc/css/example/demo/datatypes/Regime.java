00b0eb52991803d9577b0fe450a8a469bacb7e4e
fc58236 <fc58236@alunos.fc.ul.pt>
2024-05-22 01:48:14 +0100

    Refactor Regime enum and add author comments

--- server/src/main/java/pt/ul/fc/css/example/demo/datatypes/Regime.java
+++ server/src/main/java/pt/ul/fc/css/example/demo/datatypes/Regime.java
@@ -1,6 +1,13 @@
 package pt.ul.fc.css.example.demo.datatypes;
 
+/**
+ * This class represents the type of regime, from the proposal defense.
+ *
+ * @author Joao Miguel
+ * @author Guilherme Gouveia
+ * @author Rafael Correia
+ */
 public enum Regime {
-    REMOTO,
-    PRESENCIAL
+  REMOTO,
+  PRESENCIAL
 }
